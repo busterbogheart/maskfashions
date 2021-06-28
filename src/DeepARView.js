454c21ede6b1973615e8698a9819f8f6271df672
Ethan <him@ethankemp.net>
2021-06-28 15:30:11 -0400

    hi

---
+++ src/DeepARView.js
@@ -0,0 +1,13 @@
+
+import { requireNativeComponent } from 'react-native';
+import { Component } from 'react';
+
+class DeepARView extends Component {
+    render(){
+      return <p>Yup. </p>;
+    };
+}
+
+module.exports = requireNativeComponent('DeepARModule',);
+
+var DeepARView = 
