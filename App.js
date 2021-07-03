7a55ff8e81ac8e740df6c81bf03d51598aa3bda2
Ethan <him@ethankemp.net>
2021-07-03 16:43:11 -0400

    fuckin with permissions

--- App.js
+++ App.js
@@ -4,28 +4,70 @@ import DeepARView from './src/DeepARView';
 
 export default class App extends React.Component {
 
+  constructor(props){
+    super(props)
+
+    this.state = {
+      permissionsGranted: Platform.OS === 'ios',
+      currentEffectIndex: 0,
+      switchCameraInProgress: false
+    }
+  }
+
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.requestMultiple(
+        [
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
+        ]
+      ).then(result => {
+        if (
+          result['android.permission.CAMERA'] === 'granted' &&  
+          result['android.permission.WRITE_EXTERNAL_STORAGE'] === 'granted' && 
+          result['android.permission.RECORD_AUDIO'] === 'granted') {
+            this.setState({ permissionsGranted: true, showPermsAlert: false });
+        } else {
+          this.setState({ permissionsGranted: false, showPermsAlert: true });
+        }
+      })
+    }
+  }
+
+  didAppear() {
+    if (this.deepARView) {
+      this.deepARView.resume();
+    }
+  }
+
+  willDisappear(){
+    if (this.deepARView) {
+      this.deepARView.pause();
+    }
+  }
+
   render(){
+    const { permissionsGranted } = this.state
     const { width } = Dimensions.get('window')
     return (
       <View style={styles.container}>
-        <Text>WHATT</Text>
-        <DeepARView style={{borderWidth:'2px', position: 'absolute', width: '100%', height: '100%'}} />
-        <Button title="request" onPress={requestCameraPermission}></Button>
+      { permissionsGranted ? 
+        <View>
+          <DeepARView 
+            style={{width: width, height: '100%'}}
+            ref={ ref => this.deepARView = ref }
+          />
+          <Text>^ DeepAR should be above ^</Text>
+        </View>
+      : 
+      <Text>permissions not granted</Text>
+      }
+        <Text>text here.</Text>
       </View>
     );
   }
 }
-
-const requestCameraPermission = async () => {
-  PermissionsAndroid.requestMultiple(
-    [
-      PermissionsAndroid.PERMISSIONS.CAMERA,
-      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO
-    ]
-  )
-};
-
 
 
 const styles = StyleSheet.create({
