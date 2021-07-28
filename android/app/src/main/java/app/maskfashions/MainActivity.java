a4e770f01fd83e7cae859a7d47f4a9f42f1c9747
Ethan Kemp <ethankemp@Ethans-Mac-mini.local>
2021-07-28 11:35:29 -0400

    fix back button re-rendering the deepar component

--- android/app/src/main/java/app/maskfashions/MainActivity.java
+++ android/app/src/main/java/app/maskfashions/MainActivity.java
@@ -9,6 +9,11 @@ import com.facebook.react.ReactActivity;
 public class MainActivity extends ReactActivity {
 
   @Override
+  public void invokeDefaultOnBackPressed() {
+    moveTaskToBack(true);
+  }
+
+  @Override
   protected String getMainComponentName() {
     return "maskfashions";
   }
@@ -17,8 +22,6 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    //  ?? NEVER HIT
-    Log.w("DEEPAR","main activity oncreate");
     super.onCreate(savedInstanceState);
 
 //    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
@@ -26,12 +29,9 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onStart() {
-    // ?? NEVER HIT
-    Log.w("DEEPAR","main activity onstart");
     super.onStart();
     if (this.deepArView != null) {
       this.deepArView.onStart();
-      Log.w("DEEPAR","main activity -> deepar onstart");
     }
   }
 
